7c42d04ccc1fd6009f4bd7af37d782054a410f6e
Yashdeep Singh Dahiya <ydsd97@gmail.com>
2017-04-21 00:05:28 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 
-int isPalindrome(char str[])
-{
-  int len = strlen(str);
-  int i,j;
-  for(i=0,j=len-1;i<=len/2;i++,--j)
-  {
-    if(str[i]!=str[j])
-      return 0;
-  }
-  return 1;
-}
+int isPalindrome(char str[]);
+
 
 int main()
 {
@@ -24,4 +15,16 @@ int main()
   else
     printf("Not a palindome");
   return 0;
+}
+
+int isPalindrome(char str[])
+{
+  int len = strlen(str);
+  int i,j;
+  for(i=0,j=len-1;i<=len/2;i++,--j)
+  {
+    if(str[i]!=str[j])
+      return 0;
+  }
+  return 1;
 }

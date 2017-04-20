4311913314358fdfb5ffc6b7e2efb98a4999eb5d
Yashdeep Singh Dahiya <ydsd97@gmail.com>
2017-04-20 23:57:48 +0530

    I started with a main function.
    So here's the program to check whether the string entered by the user is a palindrome or not. I made a main function just to get things started. The user inputs the length of the string first. Then the string is entered. There's an if-else which checks whether the string is a palindrome by calling a isPalindrome(), which I will be making in a while.

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  char str[n];
+  scanf("%s",str);
+  if(isPalindrome(str))
+    printf("This is a palindrome");
+  else
+    printf("Not a palindome");
+  return 0;
+}
